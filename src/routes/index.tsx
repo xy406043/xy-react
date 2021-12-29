54d20e9b99baefb0e792cf5fb7207f81a19c8db2
xuyuan <xy@yuxisoft.cn>
2021-12-29 17:44:00 +0800

    :necktie: HashRouter 到chrome popup页面才可以访问默认路由

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,34 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom'
 
 import App from '@/App'
 import { SiteShow, None, Nop } from '@/pages/index'
 
 function SelfRoutes() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path="*" element={<SiteShow />}></Route>
-
-        {/* //  这种形式也无法构建SiteShow页面,如果加上Nop 默认页面也会变成Nop */}
-        {/* <Route path="/" element={<SiteShow />}></Route> */}
-        {/* <Route path="*" element={<Nop />} /> */}
-
-        {/* // 使用以下的形式，构建到chrome插件中， 默认目录一直都是 Nop 页面 */}
-        {/* <Route path="/" element={<App />}>
-          <Route index element={<SiteShow />} />
-          <Route path="*" element={<Nop />} />
-        </Route>
-        <Route path="*" element={<None />} /> */}
+        {/* // 使用 HashRouter模式才能够默认构建生效*/}
+        <Route path="/" element={<SiteShow />}></Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<SiteShow />}></Route>
+
+    //     {/* //  BrowserRouter 模式 这种形式也无法构建SiteShow页面,如果加上Nop 默认页面也会变成Nop */}
+    //     {/* <Route path="/" element={<SiteShow />}></Route> */}
+    //     {/* <Route path="*" element={<Nop />} /> */}
+
+    //     {/* // BrowRouter 模式 使用以下的形式，构建到chrome插件中， 默认目录一直都是 Nop 页面 */}
+    //     {/* <Route path="/" element={<App />}>
+    //       <Route index element={<SiteShow />} />
+    //       <Route path="*" element={<Nop />} />
+    //     </Route>
+    //     <Route path="*" element={<None />} /> */}
+    //   </Routes>
+    // </BrowserRouter>
   )
 }
 
